ef3df8b872d916a9b371b94c6c76d6829e535a1e
engine-oper <smxblysq@qq.com>
2021-07-01 17:21:33 +0800

    同步脚本

--- lib/NacosClass.py
+++ lib/NacosClass.py
@@ -53,7 +53,7 @@ class NacosClass:
         try:
             with open(yaml_achieve, 'r') as fff:
                 self.nacos.publish_config(
-                    content=yaml.dump_all(fff),
+                    content=yaml.dump_all(fff).strip('\n').strip('\r'),
                     config_type=config_type,
                     timeout=30,
                     data_id=data[-1],
