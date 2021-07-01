ccffd7658c8073f5741dbbfca8920509626071a5
engine-oper <smxblysq@qq.com>
2021-07-01 17:44:01 +0800

    同步脚本

--- lib/NacosClass.py
+++ lib/NacosClass.py
@@ -52,7 +52,7 @@ class NacosClass:
         data = yaml_achieve.split(os.sep)
         try:
             with open(yaml_achieve, 'r') as fff:
-                print(yaml.dump_all(fff))
+                print(yaml.load_all(fff, Loader=yaml.Loader))
                 self.nacos.publish_config(
                     content=yaml.dump_all(fff, allow_unicode=True),
                     config_type=config_type,
